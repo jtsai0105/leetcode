7f1ff8ad96fee21cd6ab12256ada49cbbd4a33aa
jtsai0105 <s.tsai@wustl.edu>
2018-11-25 21:33:52 -0500

    349

---
+++ a/IntersectionOfTwoArrays.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+        for (int n : nums1) {
+            set.add(n);
+        }
+        
+        Set<Integer> res = new HashSet<>();
+        for (int n : nums2) {
+            if (set.contains(n)) {
+                res.add(n);
+            }
+        }
+        
+        return res.stream().mapToInt(i -> i).toArray();
+    }
+}
