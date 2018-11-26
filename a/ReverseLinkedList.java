d1628d3547cb5940c3c91f4c3faf8a3557114740
jtsai0105 <s.tsai@wustl.edu>
2018-11-25 21:35:54 -0500

    206

---
+++ a/ReverseLinkedList.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        
+        while (head != null) {
+            ListNode tmp = head.next;
+            head.next = pre;
+            pre = head;
+            head = tmp;
+        }
+        
+        return pre;
+    }
+}
