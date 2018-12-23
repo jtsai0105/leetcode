79ae0881aa84d4d0adfd606380d264a421304e0a
jtsai0105 <s.tsai@wustl.edu>
2018-12-23 00:41:26 -0800

    852

---
+++ a/PeakIndexInAMountainArray.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int peakIndexInMountainArray(int[] A) {
+        int l = 0, r = A.length - 1, mid;
+        while (l < r) {
+            mid = l + (r - l) / 2;
+            if (A[mid] < A[mid + 1]) {
+                l = mid;
+            } else if (A[mid - 1] > A[mid]) {
+                r = mid;
+            } else {
+                return mid;
+            }
+        }
+        return 0;
+    }
+}
