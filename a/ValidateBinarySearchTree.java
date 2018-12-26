d99f61e4fc70f277aebff68990b7d972c7c5a71b
jtsai0105 <s.tsai@wustl.edu>
2018-12-25 22:59:52 -0800

    99

---
+++ a/ValidateBinarySearchTree.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    private TreeNode pre = null;
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        
+        boolean left = isValidBST(root.left);
+        if (pre != null && pre.val >= root.val) {
+            return false;
+        }
+        pre = root;
+        boolean right = isValidBST(root.right);
+        return left && right;
+    }
+}
