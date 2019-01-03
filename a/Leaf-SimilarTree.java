0cda896192783706326ec35314694d52e408ff3f
jtsai0105 <s.tsai@wustl.edu>
2019-01-03 01:09:24 -0800

    872

---
+++ a/Leaf-SimilarTree.java
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) { 
+		 return traverse(root1).equals(traverse(root2));
+     }
+	 String traverse(TreeNode root){
+		 if(root==null) return "";
+		 if(root.left==null && root.right==null) return root.val+"-";  
+		 return traverse(root.left)+traverse(root.right);
+     }
+}
