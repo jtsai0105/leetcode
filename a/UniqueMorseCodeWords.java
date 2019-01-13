a71ebad8f9f1ea6b8e51adf4a2428a1f06e126ca
jtsai0105 <s.tsai@wustl.edu>
2019-01-13 17:10:25 -0500

    804

---
+++ a/UniqueMorseCodeWords.java
@@ -0,0 +1,15 @@
+class Solution {
+    private static String[] alphCode = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+    public int uniqueMorseRepresentations(String[] words) {
+        Set<String> set = new HashSet<>();
+        for (String word : words) {
+            StringBuilder sb = new StringBuilder();
+            for (char c : word.toCharArray()) {
+                sb.append(alphCode[c - 'a']);
+            }
+            set.add(sb.toString());
+        }
+        
+        return set.size();
+    }
+}
