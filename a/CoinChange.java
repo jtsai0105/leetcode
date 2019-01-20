3512450750ee9188e61c035c781e77fae16160cd
jtsai0105 <s.tsai@wustl.edu>
2019-01-20 16:10:20 -0500

    322

---
+++ a/CoinChange.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        if (coins == null || coins.length == 0) {
+            return -1;
+        }
+        int[] dp = new int[amount + 1];
+        Arrays.sort(coins);
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for (int i = 0; i < coins.length; i++) {
+            for (int j = 1; j <= amount; j++) {
+                if (j - coins[i] >= 0) {
+                    dp[j] = Math.min(dp[j], dp[j - coins[i]] + 1);
+                }
+            }
+        }
+        
+        return dp[amount] == amount + 1 ? -1 : dp[amount];
+    }
+}
