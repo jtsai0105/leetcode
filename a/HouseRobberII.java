55dd8f51077b59941a104196817079770c0a9e8d
jtsai0105 <s.tsai@wustl.edu>
2019-02-02 12:18:50 -0800

    213 - House Robber II

--- a/HouseRobberII.java
+++ a/HouseRobberII.java
@@ -7,30 +7,18 @@ class Solution {
         if (nums.length == 1) {
             return nums[0];
         }
-        
         int n = nums.length;
-        int res1;
-        int res2;
-        int tmp = nums[0];
-        int[][] dp = new int[n - 1][2];
-        dp[0][0] = 0;
-        dp[0][1] = nums[0];
-        for (int i = 1; i < n - 1; i++) {
+        
+        return Math.max(maxRob(nums, 0, n - 2), maxRob(nums, 1, n - 1));
+    }
+    
+    private int maxRob(int[] nums, int l, int r) {
+        int[][] dp = new int[r - l + 1][2];
+        dp[0][1] = nums[l];
+        for (int i = 1; i + l <= r; i++) {
             dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1]);
-            dp[i][1] = nums[i] + dp[i - 1][0];
+            dp[i][1] = dp[i - 1][0] + nums[i + l];
         }
-        
-        res1 = Math.max(dp[n - 2][0], dp[n - 2][1]);
-        
-        dp[0][0] = 0;
-        dp[0][1] = nums[1];
-        for (int i = 1; i < n - 1; i++) {
-            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1]);
-            dp[i][1] = nums[i + 1] + dp[i - 1][0];
-        }
-        
-        res2 = Math.max(dp[n - 2][0], dp[n - 2][1]);
-        
-        return Math.max(res1, res2);
+        return Math.max(dp[r - l][0], dp[r - l][1]);
     }
 }
